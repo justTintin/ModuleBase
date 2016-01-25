ac862f52882037d88e820250f2d9b8894348a95b
justTintin <alivening@gmail.com>
2016-01-25 16:08:04 +0800

    [增加支付模块]

--- paymodule/src/main/java/com/tintin/module/pay/alipay/PayController.java
+++ paymodule/src/main/java/com/tintin/module/pay/alipay/PayController.java
@@ -7,6 +7,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.text.TextUtils;
@@ -54,6 +55,44 @@ public class PayController
                     Toast.makeText(mActivity,
                             "检查结果为：" + msg.obj,
                             Toast.LENGTH_SHORT).show();
+                    break;
+                }
+                case PayController.SDK_PAY_FLAG:
+                {
+                    PayResult payResult = new PayResult((String) msg.obj);
+
+                    // 支付宝返回此次支付结果及加签，建议对支付宝签名信息拿签约时支付宝提供的公钥做验签
+                    String resultInfo = payResult.getResult();
+
+                    String resultStatus = payResult.getResultStatus();
+
+                    // 判断resultStatus 为“9000”则代表支付成功，具体状态码代表含义可参考接口文档
+                    if (TextUtils.equals(resultStatus, "9000"))
+                    {
+                        Toast.makeText(mActivity, "支付成功", Toast.LENGTH_SHORT)
+                                .show();
+
+                    }
+                    else
+                    {
+                        // 判断resultStatus 为非“9000”则代表可能支付失败
+                        // “8000”代表支付结果因为支付渠道原因或者系统原因还在等待支付结果确认，最终交易是否成功以服务端异步通知为准（小概率状态）
+                        if (TextUtils.equals(resultStatus, "8000"))
+                        {
+                            Toast.makeText(mActivity,
+                                    "支付结果确认中",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                        else
+                        {
+                            // 其他值就可以判断为支付失败，包括用户主动取消支付，或者系统返回的错误
+                            Toast.makeText(mActivity,
+                                    "支付失败",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
                     break;
                 }
                 default:
