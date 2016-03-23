2080f6a816547afa61d135f6bef4fb2761d7b529
justTintin <alivening@gmail.com>
2016-03-23 16:09:47 +0800

    [取得cpu信息]

--- utilmodule/src/main/java/com/tintin/module/util/device/DeviceUtils.java
+++ utilmodule/src/main/java/com/tintin/module/util/device/DeviceUtils.java
@@ -7,6 +7,10 @@ import android.os.PowerManager;
 import android.telephony.TelephonyManager;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /*
 * [设备相关工具类 ]
 * [功能详细描述]
@@ -197,6 +201,33 @@ public class DeviceUtils
         }
         lastShowTime = time;
 
+    }
+
+    public static String[] getCpuInfo()
+    {
+        String str1 = "/proc/cpuinfo";
+        String str2 = "";
+        String[] cpuInfo = { "", "" };
+        String[] arrayOfString;
+        try
+        {
+            FileReader fr = new FileReader(str1);
+            BufferedReader localBufferedReader = new BufferedReader(fr, 8192);
+            str2 = localBufferedReader.readLine();
+            arrayOfString = str2.split("\\s+");
+            for (int i = 2; i < arrayOfString.length; i++)
+            {
+                cpuInfo[0] = cpuInfo[0] + arrayOfString[i] + " ";
+            }
+            str2 = localBufferedReader.readLine();
+            arrayOfString = str2.split("\\s+");
+            cpuInfo[1] += arrayOfString[2];
+            localBufferedReader.close();
+        }
+        catch (IOException e)
+        {
+        }
+        return cpuInfo;
     }
 
 }
