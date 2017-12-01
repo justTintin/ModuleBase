3b1c8e43c5cb94e804241dc621e4701d2a042b4e
borning <alivening@gmail.com>
2017-12-01 18:16:52 +0800

    修改dialog的构造方法,修改webview加载时的重新定向时的过滤

--- widgetmodule/src/main/java/me/tintin/module/widget/webview/AppWebView.java
+++ widgetmodule/src/main/java/me/tintin/module/widget/webview/AppWebView.java
@@ -3,6 +3,7 @@ package me.tintin.module.widget.webview;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.net.http.SslError;
@@ -24,25 +25,25 @@ import android.webkit.WebViewClient;
 public class AppWebView extends WebView
 {
     private static final String TAG = AppWebView.class.getSimpleName();
-
-    //JS发起请求的关键key
-    public static final String JS_OACTION_HEAD = "oAction";
-
+    
+    // JS发起请求的关键key
+    public static final String JS_OACTION_HEAD = "onAction";
+    
     private JsInteration mJsInteration;
-
+    
     private String mLoadUrl;
-
+    
     private Context mContext;
-
+    
     private OnWebViewInteractionListener mListener;
-
+    
     private WebSettings mWebSettings;
-
+    
     public AppWebView(Context context)
     {
         super(context);
     }
-
+    
     public void setAppWebView(OnWebViewInteractionListener listener,
             Context context, JsInteration jsInteration, String url)
     {
@@ -52,51 +53,52 @@ public class AppWebView extends WebView
         mListener = listener;
         initWebViewSetting();
     }
-
+    
     public AppWebView(Context context, AttributeSet attrs)
     {
         super(context, attrs);
     }
-
+    
     private void initWebViewSetting()
     {
         mWebSettings = getSettings();
-        //        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
-        //        {
-        //            WebView.setWebContentsDebuggingEnabled(true);
-        //        }
-        //设置支持JavaScript脚本
-
+        // if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+        // {
+        // WebView.setWebContentsDebuggingEnabled(true);
+        // }
+        
+        // 设置支持JavaScript脚本
         mWebSettings.setJavaScriptEnabled(true);
         addJavascriptInterface(mJsInteration, JS_OACTION_HEAD);
-        //水平不显示
+        // 水平不显示
         setHorizontalScrollBarEnabled(false);
-        //垂直不显示
+        // 垂直不显示
         setVerticalScrollBarEnabled(false);
-
-        //设置可以访问文件
+        
+        // 设置可以访问文件
         mWebSettings.setAllowFileAccess(true);
-        //设置支持缩放
+        // 设置支持缩放
         mWebSettings.setBuiltInZoomControls(true);
         mWebSettings.setUseWideViewPort(true);
         mWebSettings.setLoadWithOverviewMode(true);
         mWebSettings.setDisplayZoomControls(false);
         mWebSettings.setBlockNetworkImage(true);
         mWebSettings.setDatabaseEnabled(true);
-        //        String dir = getActivity().getApplicationContext().getDir("database", Context.MODE_PRIVATE).getPath();
-        //        mWebSettings.setDatabasePath(dir);
-
-        //打开使用缓存
+        // String dir = getActivity().getApplicationContext().getDir("database",
+        // Context.MODE_PRIVATE).getPath();
+        // mWebSettings.setDatabasePath(dir);
+        
+        // 打开使用缓存
         mWebSettings.setDomStorageEnabled(true);
         mWebSettings.setAppCacheEnabled(true);
         mWebSettings.setSaveFormData(true);
         mWebSettings.setGeolocationEnabled(true);
         mWebSettings.setRenderPriority(WebSettings.RenderPriority.HIGH);
-
-        //        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-
-        //使用缓存：
-
+        
+        // webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        
+        // 使用缓存：
+        
         if (Build.VERSION.SDK_INT >= 19)
         {
             mWebSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
@@ -105,12 +107,12 @@ public class AppWebView extends WebView
         else
         {
             mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
-
+            
         }
-        //mWebSettings.setGeolocationDatabasePath(dir);
+        // mWebSettings.setGeolocationDatabasePath(dir);
         setWebChromeClient(new android.webkit.WebChromeClient()
         {
-
+            
             @Override
             public void onProgressChanged(WebView view, int newProgress)
             {
@@ -123,173 +125,200 @@ public class AppWebView extends WebView
                 {
                     mListener.onWebViewLoadStart();
                 }
-
+                
             }
-
+            
             @Override
             public boolean onJsAlert(WebView view, String url, String message,
                     JsResult result)
             {
                 return super.onJsAlert(view, url, message, result);
-
+                
             }
-
+            
             @Override
-            public boolean onJsConfirm(WebView view, String url,
-                    String message, JsResult result)
+            public boolean onJsConfirm(WebView view, String url, String message,
+                    JsResult result)
             {
                 return super.onJsConfirm(view, url, message, result);
             }
-
-            //关键代码，以下函数是没有API文档的，所以在Eclipse中会报错，如果添加了@Override关键字在这里的话。
-
+            
+            // 关键代码，以下函数是没有API文档的，所以在Eclipse中会报错，如果添加了@Override关键字在这里的话。
+            
             // For Android 3.0+
-
+            
             public void openFileChooser(ValueCallback<Uri> uploadMsg)
             {
                 mListener.openFileChooser(uploadMsg);
-
+                
             }
-
+            
             // For Android 3.0+
             public void openFileChooser(ValueCallback uploadMsg,
                     String acceptType)
             {
                 mListener.openFileChooser(uploadMsg, acceptType);
-
+                
             }
-
-            //For Android 4.1
+            
+            // For Android 4.1
             public void openFileChooser(ValueCallback<Uri> uploadMsg,
                     String acceptType, String capture)
             {
                 mListener.openFileChooser(uploadMsg, acceptType, capture);
-
+                
             }
-
-            //Android 5.0+
+            
+            // Android 5.0+
             @Override
             @SuppressLint("NewApi")
             public boolean onShowFileChooser(WebView webView,
                     ValueCallback<Uri[]> filePathCallback,
                     FileChooserParams fileChooserParams)
             {
-
+                
                 return mListener.onShowFileChooser(webView,
                         filePathCallback,
                         fileChooserParams);
             }
-
+            
         });
-
-        //设置WebViewClient
+        
+        // 设置WebViewClient
         setWebViewClient(new WebViewClient()
         {
-
+            
             public void onReceivedSslError(WebView view,
                     SslErrorHandler handler, SslError error)
             {
-                //handler.cancel(); // Android默认的处理方式
+                // handler.cancel(); // Android默认的处理方式
                 handler.proceed(); // 接受所有网站的证书
-                //handleMessage(Message msg); // 进行其他处理
+                // handleMessage(Message msg); // 进行其他处理
             }
-
+            
             public boolean shouldOverrideUrlLoading(WebView view, String url)
             {
-                view.loadUrl(url);
+                if (url == null)
+                    return false;
+                
+                try
+                {
+                    if (url.startsWith("http:") || url.startsWith("https:")
+                            || url.startsWith("file:"))
+                    {
+                        view.loadUrl(url);
+                    }
+                    else if (url.startsWith("weixin://")
+                            || url.startsWith("alipays://")
+                            || url.startsWith("mailto://")
+                            || url.startsWith("tel://"))
+                    {
+                        // 其它类型的scheme的处理
+                        Intent intent = new Intent(Intent.ACTION_VIEW,
+                                Uri.parse(url));
+                        mContext.startActivity(intent);
+                    }
+                    
+                }
+                catch (Exception e)
+                {
+                    // 防止crash,如果手机上没有安装 某个 scheme开头的url的app,会导致cr
+                    return false;
+                }
                 return true;
+                
             }
-
+            
             public void onPageFinished(WebView view, String url)
             {
-
+                
                 super.onPageFinished(view, url);
             }
-
+            
             public void onPageStarted(WebView view, String url, Bitmap favicon)
             {
                 super.onPageStarted(view, url, favicon);
-
+                
             }
-
+            
         });
-
-        //判断当前URL是否有效
-        preLoadUrl(mLoadUrl);
-
+        
+        // 判断当前URL是否有效
+        // preLoadUrl(mLoadUrl);
+        loadUrl(mLoadUrl);
     }
-
+    
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public AppWebView(Context context, AttributeSet attrs, int defStyleAttr,
             int defStyleRes)
     {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
-
+    
     public AppWebView(Context context, AttributeSet attrs, int defStyleAttr)
     {
         super(context, attrs, defStyleAttr);
     }
-
+    
     /**
      * 预请求
+     * 
      * @param loadUrl
      */
     public void preLoadUrl(final String loadUrl)
     {
-        //        //创建一个Request
-        //        final Request request = new Request.Builder().url(loadUrl).build();
+        // //创建一个Request
+        // final Request request = new Request.Builder().url(loadUrl).build();
         //
-        //        //请求加入调度
-        //        OkHttpUtils.enqueue(request, new Callback()
-        //        {
-        //            @Override
-        //            public void onFailure(Request request, IOException e)
-        //            {
-        //                Logger.i(TAG, "onFailure:" + loadUrl + "head:"
-        //                        + request.headers().toString());
-        //                mListener.onWebViewPreLoadFailed(CommonNetConstant.CODE.ERROR_CODE_SEVER);
-        //            }
+        // //请求加入调度
+        // OkHttpUtils.enqueue(request, new Callback()
+        // {
+        // @Override
+        // public void onFailure(Request request, IOException e)
+        // {
+        // Logger.i(TAG, "onFailure:" + loadUrl + "head:"
+        // + request.headers().toString());
+        // mListener.onWebViewPreLoadFailed(CommonNetConstant.CODE.ERROR_CODE_SEVER);
+        // }
         //
-        //            @Override
-        //            public void onResponse(final Response response) throws IOException
-        //            {
-        //                if (response.code() == CommonNetConstant.CODE.OK_CODE_200)
-        //                {
+        // @Override
+        // public void onResponse(final Response response) throws IOException
+        // {
+        // if (response.code() == CommonNetConstant.CODE.OK_CODE_200)
+        // {
         //
-        //                    mListener.onWebViewPreLoadSuccess(loadUrl);
-        //                    //loadDataWithBaseURL(mLoadUrl);
+        // mListener.onWebViewPreLoadSuccess(loadUrl);
+        // //loadDataWithBaseURL(mLoadUrl);
         //
-        //                }
-        //                else
-        //                {
-        //                    mListener.onWebViewPreLoadFailed(response.code());
-        //                }
+        // }
+        // else
+        // {
+        // mListener.onWebViewPreLoadFailed(response.code());
+        // }
         //
-        //            }
-        //        });
-
+        // }
+        // });
+        
     }
-
+    
     public interface OnWebViewInteractionListener
     {
         void onWebViewLoadFinish();
-
+        
         void onWebViewLoadStart();
-
+        
         void onWebViewPreLoadFailed(int errorCode);
-
+        
         void onWebViewPreLoadSuccess(String url);
-
+        
         void openFileChooser(ValueCallback<Uri> uploadMsg);
-
+        
         void openFileChooser(ValueCallback uploadMsg, String acceptType);
-
+        
         void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType,
                 String capture);
-
-        boolean onShowFileChooser(
-                WebView webView,
+        
+        boolean onShowFileChooser(WebView webView,
                 ValueCallback<Uri[]> filePathCallback,
                 android.webkit.WebChromeClient.FileChooserParams fileChooserParams);
     }
--- widgetmodule/src/main/java/me/tintin/module/widget/webview/JsInteration.java
+++ widgetmodule/src/main/java/me/tintin/module/widget/webview/JsInteration.java
@@ -14,46 +14,50 @@ import android.webkit.JavascriptInterface;
 public class JsInteration
 {
     private static final String TAG = JsInteration.class.getSimpleName();
-
+    
     private Handler mHandler;
-
+    
     private Activity mActivity;
-
+    
+    public static final int MSG_JS_STR = 999;
+    
     /**
      * 绑定的object对象
      */
-    //    private Context mContext;
-
+    // private Context mContext;
+    
     public JsInteration(Activity activity, Handler handler)
     {
         this.mActivity = activity;
         this.mHandler = handler;
     }
-
+    
     /*
      * JS调用android的方法
+     * 
      * @JavascriptInterface仍然必不可少
      *
-     * */
+     */
     @JavascriptInterface
     public void postMessage(String jsonStr)
     {
-        String curUrl = null;
         try
         {
-            callJsMethod();
+            callJsMethod(jsonStr);
         }
         catch (Exception e)
         {
             e.printStackTrace();
         }
-
+        
     }
-
-    private void callJsMethod()
+    
+    private void callJsMethod(String jsonStr)
     {
         Message msg = mHandler.obtainMessage();
+        msg.what = MSG_JS_STR;
+        msg.obj = jsonStr;
         mHandler.sendMessage(msg);
-
+        
     }
 }
